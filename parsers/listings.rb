be005183be08c7fb441ca5a4cdeebaac5ba47b2d
Ahmet Fahad <ahmadfahad6394@gmail.com>
2021-03-17 23:39:18 +0300

    test

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -3,6 +3,16 @@ nokogiri = Nokogiri.HTML(content)
 products = nokogiri.at_css("li.list-item")
 
 products.each do |product|
-  
+  title = product.css(".item-title").text
+  url = product.css(".place-container a").attr('href').text
+
+  pages{
+    url: url,
+    page_type: 'product',
+    vars: {
+      'title' => title
+    }
+
+  }
 
 end  
