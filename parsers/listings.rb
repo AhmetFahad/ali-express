e4d1470887689367520f8698419dbcb80ee8c514
Ahmet Fahad <ahmadfahad6394@gmail.com>
2021-03-19 20:59:18 +0300

    add listings parser to config

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -22,11 +22,10 @@ end
 
 
 page_number = html.css('.next-pagination-pages .next-pagination-list button').last.text.to_i
-// write your code to scrape product list here
 if vars['page'] < page_number
   pages << {
     url: "https://www.aliexpress.com/category/7/computer-office.html?trafficChannel=main&catName=computer-office&CatId=7&ltype=wholesale&SortType=default&page=#{vars['page']+1}&isrefine=y",
-   // write rest of the code
+    page_type: 'listings'
    vars: {
      page_number: vars['page_number'] + 1
    }
